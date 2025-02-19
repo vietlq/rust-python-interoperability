b32bfdce01a1e750f94aac54fbd68a66fff96cbc
Le Quoc Viet <vietlq85@gmail.com>
2025-02-19 10:18:23 +0000

    added notes for the name of generated python module

--- exercises/01_intro/02_modules/src/lib.rs
+++ exercises/01_intro/02_modules/src/lib.rs
@@ -7,7 +7,8 @@ fn it_works() -> bool {
 }
 
 /// A Python module implemented in Rust.
-#[pymodule]
+/// The fix: We added `name="modules"` here to customise the name of the Python module
+#[pymodule(name="modules")]
 fn learn_modules(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(it_works, m)?)?;
     Ok(())
