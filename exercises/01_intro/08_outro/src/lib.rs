033acfb5dfa04af82cda5039070f64913ea8ad6b
Le Quoc Viet <vietlq85@gmail.com>
2025-02-19 14:02:18 +0000

    basic structure for new module outro1::max_k

--- exercises/01_intro/08_outro/src/lib.rs
+++ exercises/01_intro/08_outro/src/lib.rs
@@ -2,3 +2,33 @@
 //   a list containing the `k` largest numbers in the list, in descending order.
 //
 // Hint: you can use the `num_bigint` crate if you think it'd be useful.
+
+use pyo3::exceptions::{PyTypeError, PyValueError};
+use pyo3::prelude::*;
+use pyo3::types::PyList;
+
+#[pyfunction]
+fn max_k<'py>(
+    input_list: Vec<u64>,
+    num_to_take: u64,
+    py: Python<'py>,
+) -> PyResult<Bound<'py, PyList>> {
+    if num_to_take > (input_list.len() as u64) {
+        Err(PyValueError::new_err(
+            "The list has fewer elements than requested",
+        ))
+    } else {
+        if num_to_take == 0 {
+            let empty_vec: Vec<u64> = vec![];
+            Ok(PyList::new(py, empty_vec)?)
+        } else {
+            Ok(PyList::new(py, input_list)?)
+        }
+    }
+}
+
+#[pymodule]
+fn outro1(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(max_k, m)?)?;
+    Ok(())
+}
