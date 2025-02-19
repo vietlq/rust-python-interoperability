187f4a9c4ac0c7b5d30f56dce66e1b64097ffdab
Le Quoc Viet <vietlq85@gmail.com>
2025-02-19 14:19:16 +0000

    reorg code for brevity

--- exercises/01_intro/08_outro/src/lib.rs
+++ exercises/01_intro/08_outro/src/lib.rs
@@ -13,26 +13,25 @@ fn max_k<'py>(
     num_to_take: u64,
     py: Python<'py>,
 ) -> PyResult<Bound<'py, PyList>> {
-    let list_len = (input_list.len() as u64);
-    if num_to_take > list_len {
+    let list_len = input_list.len() as u64;
+
+    if num_to_take == 0 {
+        let empty_vec: Vec<u64> = vec![];
+        Ok(PyList::new(py, empty_vec)?)
+    } else if num_to_take > list_len {
         Err(PyValueError::new_err(
             "The list has fewer elements than requested",
         ))
     } else {
-        if num_to_take == 0 {
-            let empty_vec: Vec<u64> = vec![];
-            Ok(PyList::new(py, empty_vec)?)
-        } else {
-            let mut input_list = input_list.clone();
-            input_list.sort_by(|a, b| a.cmp(b));
-            input_list.reverse();
+        let mut input_list = input_list.clone();
+        input_list.sort_by(|a, b| a.cmp(b));
+        input_list.reverse();
 
-            if num_to_take == list_len {
-                Ok(PyList::new(py, input_list)?)
-            } else {
-                let (left, _right) = input_list.split_at(num_to_take as usize);
-                Ok(PyList::new(py, left.to_vec())?)
-            }
+        if num_to_take == list_len {
+            Ok(PyList::new(py, input_list)?)
+        } else {
+            let (left, _right) = input_list.split_at(num_to_take as usize);
+            Ok(PyList::new(py, left.to_vec())?)
         }
     }
 }
