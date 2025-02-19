cf161ef2f0df276e177f7f6029179a781ea04cf8
Le Quoc Viet <vietlq85@gmail.com>
2025-02-19 15:21:36 +0000

    implemented ShoppingOrder::new

--- exercises/02_classes/01_constructors/src/lib.rs
+++ exercises/02_classes/01_constructors/src/lib.rs
@@ -1,4 +1,6 @@
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
+use pyo3::types::PyInt;
 
 // TODO: Add a `__new__` constructor to the `ShoppingOrder` class that takes the following arguments:
 //  - `name` (non-empty string)
@@ -10,10 +12,37 @@ use pyo3::prelude::*;
 struct ShoppingOrder {
     #[pyo3(get)]
     name: String,
+
     #[pyo3(get)]
     price: u64,
+
     #[pyo3(get, set)]
     quantity: u64,
+}
+
+#[pymethods]
+impl ShoppingOrder {
+    #[new]
+    fn new<'py>(
+        name: String,
+        price: Bound<'py, PyInt>,
+        quantity: Bound<'py, PyInt>,
+        py: Python<'py>,
+    ) -> PyResult<ShoppingOrder> {
+        let res_price = price.extract::<u64>();
+        let res_quantity = quantity.extract::<u64>();
+
+        match (res_price, res_quantity) {
+            (Ok(price), Ok(quantity)) => Ok(ShoppingOrder {
+                name,
+                price,
+                quantity,
+            }),
+            _ => Err(PyValueError::new_err(
+                "price and quantity must be of the type u64",
+            )),
+        }
+    }
 }
 
 #[pymodule]
