da44de45e42695f1112b0dfcae8e916b4f81049f
Viet Le <vietlq85@gmail.com>
2025-08-31 15:24:19 +0100

    04_arguments: implemented print_number_list

--- exercises/01_intro/04_arguments/src/lib.rs
+++ exercises/01_intro/04_arguments/src/lib.rs
@@ -3,7 +3,18 @@ use pyo3::prelude::*;
 #[pyfunction]
 // TODO: Define a function that takes as input a vector of unsigned integers
 //  and prints each number in the list.
-fn print_number_list() {}
+fn print_number_list(xs: Vec<u64>) {
+    // This works but we do not want to keep the variables
+    /*
+    _ = xs.iter().map(|x| println!("{}", x)).collect::<Vec<_>>();
+    ()
+    */
+
+    // This solution is simpler and uses less memory
+    for x in xs {
+        println!("{}", x)
+    }
+}
 
 #[pymodule]
 fn arguments(m: &Bound<'_, PyModule>) -> PyResult<()> {
