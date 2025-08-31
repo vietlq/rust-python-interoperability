72b00ce0a16256651707c2a0627307c4102b5ed0
Viet Le <vietlq85@gmail.com>
2025-08-31 22:09:03 +0100

    06_output: iterative fibonacci

--- exercises/01_intro/06_output/src/lib.rs
+++ exercises/01_intro/06_output/src/lib.rs
@@ -2,8 +2,32 @@ use pyo3::prelude::*;
 
 #[pyfunction]
 // TODO: Implement a function that returns a list containing the first `n` numbers in Fibonacci's sequence.
-fn fibonacci(/* n: TODO */) /* -> TODO */ {
-    todo!()
+fn fibonacci(n: u8) -> Vec<u64> {
+    if n == 0 {
+        return vec![];
+    }
+
+    if n == 1 {
+        return vec![0];
+    }
+
+    let mut result: Vec<u64> = vec![0, 1];
+    let mut runner: u8 = 2;
+    let mut prev: u64 = 1;
+    let mut curr: u64 = 1;
+
+    // Avoiding recursion for better speed and memory utilisation
+    while runner < n {
+        result.push(curr.into());
+        runner += 1;
+
+        let new_prev = curr.clone();
+        let new_curr = prev + curr;
+        prev = new_prev;
+        curr = new_curr;
+    }
+
+    result
 }
 
 #[pymodule]
