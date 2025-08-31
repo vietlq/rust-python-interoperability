28c5fcb21815907d9ea6a9ef4f233f2e138f46ff
Viet Le <vietlq85@gmail.com>
2025-08-31 15:37:00 +0100

    05_gil: use Bound + PyList + extract()

--- exercises/01_intro/05_gil/src/lib.rs
+++ exercises/01_intro/05_gil/src/lib.rs
@@ -1,10 +1,14 @@
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::PyList};
 
 #[pyfunction]
 // TODO: Use `PyList` instead of `Vec<u64>` as the input type. Panic on errors, for now.
 // You might find this useful: https://pyo3.rs/v0.22.0/conversions/traits#extract-and-the-frompyobject-trait
-fn print_number_list() {
-    todo!()
+fn print_number_list(pxs: &Bound<'_, PyList>) {
+    let xs: Vec<u64> = pxs.extract().unwrap();
+
+    for x in xs {
+        println!("{}", x)
+    }
 }
 
 #[pymodule]
