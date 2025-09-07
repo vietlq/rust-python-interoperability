47bf67fd5abcaad9741a2e25e7ae48753387f725
Viet Le <vietlq85@gmail.com>
2025-09-07 22:18:53 +0100

    added skeleton + tested with nvim + lsp_lines

--- exercises/01_intro/08_outro/src/lib.rs
+++ exercises/01_intro/08_outro/src/lib.rs
@@ -2,3 +2,18 @@
 //   a list containing the `k` largest numbers in the list, in descending order.
 //
 // Hint: you can use the `num_bigint` crate if you think it'd be useful.
+use num_bigint::BigUint;
+use pyo3::exceptions::PyTypeError;
+use pyo3::prelude::*;
+use pyo3::types::PyList;
+
+#[pyfunction]
+fn max_k(xs: Vec<BigUint>, k: usize) -> Vec<BigUint> {
+    xs.iter().take(k).collect()
+}
+
+#[pymodule]
+fn outro1(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(max_k, m)?)?;
+    Ok(())
+}
