a24fa2fdb29ceb543a95e9a35ef1606bfe4023ba
Viet Le <vietlq85@gmail.com>
2025-09-13 21:56:15 +0100

    grammar

--- exercises/02_classes/02_methods/src/lib.rs
+++ exercises/02_classes/02_methods/src/lib.rs
@@ -23,14 +23,14 @@ impl ShoppingOrder {
     fn new(name: String, price: Bound<'_, PyInt>, quantity: Bound<'_, PyInt>) -> PyResult<Self> {
         let price = price
             .extract()
-            .map_err(|_| PyValueError::new_err("Price must be an non-negative integer"))?;
+            .map_err(|_| PyValueError::new_err("Price must be a non-negative integer"))?;
         if price == 0 {
             return Err(PyValueError::new_err("Price cannot be zero"));
         }
 
         let quantity = quantity
             .extract()
-            .map_err(|_| PyValueError::new_err("Quantity must be an non-negative integer"))?;
+            .map_err(|_| PyValueError::new_err("Quantity must be a non-negative integer"))?;
         if quantity == 0 {
             return Err(PyValueError::new_err("Quantity cannot be zero"));
         }
