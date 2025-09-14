8632ddf920f83b77fdb90e5811df8c35c84f9596
Viet Le <vietlq85@gmail.com>
2025-09-14 06:48:28 +0100

    [04_static_methods] added @staticmethod and @classmethod

--- exercises/02_classes/04_static_methods/src/lib.rs
+++ exercises/02_classes/04_static_methods/src/lib.rs
@@ -1,5 +1,7 @@
+// https://rust-exercises.com/rust-python-interop/02_classes/04_static_methods.html
 // TODO: Add an additional method to the `Discount` class to return a default 10% discount.
 use pyo3::prelude::*;
+use pyo3::types::PyType;
 
 #[pyclass]
 struct Discount {
@@ -12,6 +14,19 @@ impl Discount {
     #[new]
     fn new(percentage: f64) -> Self {
         Discount { percentage }
+    }
+
+    #[staticmethod]
+    fn default() -> Self {
+        Discount::new(0.1)
+    }
+
+    // Notice the cls argument
+    // Since we can refer to Self type in #[staticmethod] in Rust,
+    // we don't usually need classmethod like in Python.
+    #[classmethod]
+    fn default_class_method(_cls: &Bound<'_, PyType>) -> Self {
+        Discount::new(0.1)
     }
 }
 
