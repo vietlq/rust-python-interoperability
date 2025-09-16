a1b414a2bb80c165a999f3e66c14aeabff08b940
Viet Le <vietlq85@gmail.com>
2025-09-16 07:51:29 +0100

    [05_immutable_types] use #[pyclass(frozen)]

--- exercises/03_concurrency/05_immutable_types/src/lib.rs
+++ exercises/03_concurrency/05_immutable_types/src/lib.rs
@@ -1,6 +1,55 @@
+/*
+* https://rust-exercises.com/rust-python-interop/03_concurrency/05_immutable_types.html
+*
+* Let's see how we can define a similar immutable type in Rust.
+
 use pyo3::prelude::*;
 
-#[pyclass]
+#[pyclass(frozen)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+The above is not enough to get all the niceties of Python's dataclasses,
+but it's sufficient to make the class immutable.
+If a pyclass is marked as frozen, pyo3 will allow us to access its fields
+without holding the GIL—i.e. via Py<T> instead of Bound<'py, T>
+
+#[pyfunction]
+fn print_point<'py>(python: Python<'py>, point: Bound<'py, Point>) {
+    let point: Py<Point> = point.unbind();
+    python.allow_threads(|| {
+        // We can now access the fields of the Point struct
+        // even though we are not holding the GIL
+        let point: &Point = point.get();
+        println!("({}, {})", point.x, point.y);
+    });
+}
+
+This wouldn't compile if Point wasn't marked as frozen,
+thanks to Py<T>::get's signature:
+
+impl<T> Py<T>
+where
+    T: PyClass,
+{
+    pub fn get(&self) -> &T
+    where
+        // `Frozen = True` is where the magic happens!
+        T: PyClass<Frozen = True> + Sync,
+    { /* ... */ }
+}
+
+Summary
+
+Immutable types significantly simplify GIL jugglery in pyo3. If it fits the
+constraints of the problem you're solving, consider using them to make
+your code easier to reason about (and potentially faster!).
+* */
+use pyo3::prelude::*;
+
+#[pyclass(frozen)]
 struct Rectangle {
     width: u32,
     length: u32,
@@ -11,6 +60,10 @@ impl Rectangle {
     #[new]
     fn new(width: u32, length: u32) -> Self {
         Self { width, length }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.length
     }
 }
 
@@ -24,8 +77,12 @@ impl Rectangle {
 /// Do NOT remove the `allow_threads` call. The computation must be done inside
 /// the closure passed to `allow_threads`.
 fn compute_area<'py>(python: Python<'py>, shape: Bound<'py, Rectangle>) -> u32 {
+    // We must unbind before entering Python::allow_threads()
+    let shape = shape.unbind();
+
     python.allow_threads(|| {
-        let area: u32 = todo!();
+        // Thanks to ``#[pyclass(frozen)]`, we can use `shape.get()` here.
+        let area: u32 = shape.get().area();
         area
     })
 }
